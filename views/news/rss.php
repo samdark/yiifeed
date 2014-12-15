07afd90fec9ffc2f92471d763cc0c92aa2008901
Alexander Makarov <sam@rmcreative.ru>
2014-12-16 00:35:18 +0300

    Improved RSS interoperability

--- views/news/rss.php
+++ views/news/rss.php
@@ -2,17 +2,21 @@
 use yii\helpers\Markdown;
 use yii\helpers\Url;
 use yii\helpers\Html;
-/* @var $this yii\web\View */?>
-<?= '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL ?>
-<rss version="2.0">
+/* @var $this yii\web\View */
+echo '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
+?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
-        <title>Yiifeed News</title>
+        <title>YiiFeed News</title>
         <link><?= Url::base(true) ?></link>
-        <description>Last news from YiiFeed</description>
+        <atom:link rel="self" href="<?= Url::to([''], true) ?>" />
+        <description>Everything about Yii framework</description>
+        <language>en</language>
         <?php foreach ($news as $item): ?>
             <item>
                 <title><?= Html::encode($item->title) ?></title>
-                <description><?= strip_tags(Markdown::process($item->text)) ?></description>
+                <description><![CDATA[<?= Html::encode(Markdown::process($item->text)) ?>]]></description>
+                <guid isPermaLink="false"><![CDATA[<?= Url::to(['news/view', 'id' => $item->id], true)?>]]></guid>
                 <pubDate><?= date("D, j M Y G:i:s", $item->created_at). " GMT" ?></pubDate>
             </item>
         <?php endforeach ?>
