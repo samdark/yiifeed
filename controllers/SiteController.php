f2a25c179b618dd20a37aeda2876ee9e308176f3
VadimTs <vadimhm@yandex.ru>
2014-12-17 01:26:23 +0200

    add role when signup

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -178,6 +178,9 @@ class SiteController extends Controller
         if ($model->load(Yii::$app->request->post())) {
             if ($user = $model->signup()) {
                 if (Yii::$app->getUser()->login($user)) {
+                    $auth = Yii::$app->authManager;
+                    $authorRole = $auth->getRole('user');
+                    $auth->assign($authorRole, $user->getId());
                     return $this->goHome();
                 }
             }
