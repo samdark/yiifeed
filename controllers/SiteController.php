8a1ad76987454ed4622bdf0e50081c34bfaa38b3
Alexander Makarov <sam@rmcreative.ru>
2014-12-23 00:41:45 +0300

    Added ability to connect extra profiles to the same account

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -23,10 +23,10 @@ class SiteController extends Controller
         return [
             'access' => [
                 'class' => AccessControl::className(),
-                'only' => ['logout', 'signup', 'auth'],
+                'only' => ['logout', 'signup'],
                 'rules' => [
                     [
-                        'actions' => ['signup', 'auth'],
+                        'actions' => ['signup'],
                         'allow' => true,
                         'roles' => ['?'],
                     ],
--- views/user/view.php
+++ views/user/view.php
@@ -15,20 +15,37 @@ use \yii\widgets\ListView;
 ?>
 <div class="row user-view">
     <div class="col-xs-12">
-        <h1><?= Html::encode($this->title) ?></h1>
+        <div class="row">
+            <div class="col-xs-6">
+                <h1><?= Html::encode($this->title) ?></h1>
 
-        <?php if (Yii::$app->user->can('admin')): ?>
-        <p>
-            <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-            <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-                'class' => 'btn btn-danger',
-                'data' => [
-                    'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                    'method' => 'post',
-                ],
-            ]) ?>
-        </p>
-        <?php endif ?>
+                <?php if (Yii::$app->user->can('admin')): ?>
+                <p>
+                    <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+                    <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+                        'class' => 'btn btn-danger',
+                        'data' => [
+                            'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                            'method' => 'post',
+                        ],
+                    ]) ?>
+                </p>
+                <?php endif ?>
+            </div>
+            <?php if (Yii::$app->user->id == $model->id): ?>
+            <div class="col-xs-6">
+                <h2>Connect extra profiles:</h2>
+                <?= yii\authclient\widgets\AuthChoice::widget([
+                    'baseAuthUrl' => ['site/auth']
+                ]) ?>
+            </div>
+            <?php endif ?>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+
+            </div>
+        </div>
 
         <?= ListView::widget([
             'dataProvider' => $dataProvider,
