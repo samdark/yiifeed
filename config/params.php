fda51437fa4b18e95066a748b3bab30f1322fe09
Alexander Makarov <sam@rmcreative.ru>
2014-12-24 22:24:03 +0300

    Set emails

--- config/params.php
+++ config/params.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+    'adminEmail' => 'sam+yiifeed@rmcreative.ru',
+    'supportEmail' => 'noreply@yiifeed.com',
     'user.passwordResetTokenExpire' => 3600,
 ];
