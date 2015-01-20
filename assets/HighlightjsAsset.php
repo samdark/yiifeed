8a0289c43d0333e2d887a231f8ed8855455ecec2
Maxim Tugaev <tugmaks@yandex.ru>
2015-01-20 16:38:47 +0400

    Issue #7 Code Highlighting small fix

--- assets/HighlightjsAsset.php
+++ assets/HighlightjsAsset.php
@@ -1,19 +1,9 @@
 <?php
 
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
-
 namespace app\assets;
 
 use yii\web\AssetBundle;
 
-/**
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
 class HighlightjsAsset extends AssetBundle
 {
 
