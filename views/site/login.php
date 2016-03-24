5120edba0d9714c7ea229555502f534326f9b35e
Bulat Damdinov <dbulats88@gmail.com>
2016-03-24 12:35:15 +0800

    Updated login page for responsive

--- views/site/login.php
+++ views/site/login.php
@@ -13,23 +13,23 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <div class="row">
-        <div class="col-xs-3 col-xs-offset-1">
+        <div class="col-sm-3 col-sm-offset-1">
             <?= yii\authclient\widgets\AuthChoice::widget([
                 'baseAuthUrl' => ['site/auth'],
                 'popupMode' => false,
             ]) ?>
         </div>
-        <div class="col-xs-2">
+        <div class="col-sm-2">
             <h2>OR</h2>
         </div>
-        <div class="col-xs-5">
+        <div class="col-sm-5">
             <h2>fill out the following form</h2>
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
                 <?= $form->field($model, 'username') ?>
                 <?= $form->field($model, 'password')->passwordInput() ?>
-                <div style="color:#999;margin:1em 0">
+                <p class="hint-block">
                     If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                </div>
+                </p>
                 <div class="form-group">
                     <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
                 </div>
