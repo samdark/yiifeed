776943ddf71e69e2e89a2d4266906fad8e638336
Bulat Damdinov <dbulats88@gmail.com>
2016-03-26 09:44:08 +0800

    Signup page for responsive

--- views/site/signup.php
+++ views/site/signup.php
@@ -13,16 +13,16 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <div class="row">
-        <div class="col-xs-3 col-xs-offset-1">
+        <div class="col-sm-3 col-sm-offset-1">
             <?= yii\authclient\widgets\AuthChoice::widget([
                 'baseAuthUrl' => ['site/auth'],
                 'popupMode' => false,
             ]) ?>
         </div>
-        <div class="col-xs-2">
+        <div class="col-sm-2">
             <h2>OR</h2>
         </div>
-        <div class="col-xs-5">
+        <div class="col-sm-5">
             <h2>fill out the following form</h2>
             <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
                 <?= $form->field($model, 'username') ?>
