5af4a6b8eeff7652fa6133e5a58f32b94af9b8de
Bulat Damdinov <dbulats88@gmail.com>
2016-08-17 19:26:12 +0800

    fixed news _comments, _form, suggest files

--- views/news/_comments.php
+++ views/news/_comments.php
@@ -1,49 +1,49 @@
 <?php
-/* @var $comments app\models\Comment[] */
-/* @var $form yii\widgets\ActiveForm */
-/* @var $commentForm app\models\Comment */
-?>
 
-<?php
 use app\widgets\Avatar;
 use yii\helpers\Markdown;
 use yii\widgets\ActiveForm;
 use yii\helpers\Html;
-use \yii\helpers\HtmlPurifier;
+use yii\helpers\HtmlPurifier;
 
-\app\assets\CodeMirrorAsset::register($this);
+/**
+ * @var app\models\Comment[] $comments
+ * @var yii\widgets\ActiveForm $form
+ * @var app\models\Comment $commentForm
+ */
+
 \app\assets\MarkdownEditorAsset::register($this);
 ?>
 
 <h2>Comments</h2>
 
 <ol class="comments">
-<?php foreach ($comments as $comment): ?>
-    <li id="c<?= $comment->id ?>" class="row">
-        <div class="col-xs-1 author">
-            <?= Html::a(Avatar::widget(['user' => $comment->user]) . ' ' . Html::encode($comment->user->username), ['user/view', 'id' => $comment->user->id]) ?>
-        </div>
-        <div class="col-xs-8 text well">
-            <?= HtmlPurifier::process(Markdown::process($comment->text, 'gfm-comment'), [
-                'HTML.SafeIframe' => true,
-                'URI.SafeIframeRegexp' => '%^(https?:)?//(www\.youtube(?:-nocookie)?\.com/embed/|player\.vimeo\.com/video/)%',
-            ]) ?>
-        </div>
-        <div class="col-xs-3">
-            <a href="#c<?= $comment->id ?>">#<?= $comment->id ?></a>
-            <span class="date"><?=Yii::$app->formatter->format($comment->created_at, 'datetime')?></span>
-        </div>
-    </li>
-<?php endforeach ?>
+    <?php foreach ($comments as $comment): ?>
+        <li id="c<?= $comment->id ?>" class="row">
+            <div class="col-xs-1 author">
+                <?= Html::a(Avatar::widget(['user' => $comment->user]) . ' ' . Html::encode($comment->user->username), ['user/view', 'id' => $comment->user->id]) ?>
+            </div>
+            <div class="col-xs-8 text well">
+                <?= HtmlPurifier::process(Markdown::process($comment->text, 'gfm-comment'), [
+                    'HTML.SafeIframe' => true,
+                    'URI.SafeIframeRegexp' => '%^(https?:)?//(www\.youtube(?:-nocookie)?\.com/embed/|player\.vimeo\.com/video/)%',
+                ]) ?>
+            </div>
+            <div class="col-xs-3">
+                <a href="#c<?= $comment->id ?>">#<?= $comment->id ?></a>
+                <span class="date"><?= Yii::$app->formatter->format($comment->created_at, 'datetime') ?></span>
+            </div>
+        </li>
+    <?php endforeach ?>
 </ol>
 
 <?php if (!Yii::$app->user->isGuest): ?>
     <?php $form = ActiveForm::begin(); ?>
-        <?= $form->field($commentForm, 'text')->label('Add new comment')->textarea() ?>
+    <?= $form->field($commentForm, 'text')->label('Add new comment')->textarea() ?>
 
-        <div class="form-group">
-            <?= Html::submitButton(Yii::t('news', 'Comment'), ['class' => 'btn btn-primary']) ?>
-        </div>
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('news', 'Comment'), ['class' => 'btn btn-primary']) ?>
+    </div>
     <?php ActiveForm::end(); ?>
 <?php else: ?>
     <p>Signup in order to comment.</p>
--- views/news/_form.php
+++ views/news/_form.php
@@ -1,21 +1,27 @@
 <?php
-\app\assets\CodeMirrorAsset::register($this);
-\app\assets\MarkdownEditorAsset::register($this);
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use ijackua\lepture\Markdowneditor;
-/* @var $this yii\web\View */
-/* @var $model app\models\News */
-/* @var $form yii\widgets\ActiveForm */
-?>
-    <?php $form = ActiveForm::begin(['id' => 'news-form']); ?>
-    <?= $form->field($model, 'title')->textInput(['maxlength' => 50]) ?>
-    <?= $form->field($model, 'text')->textarea() ?>
 
-    <?= $form->field($model, 'link')->textInput(['maxlength' => 255]) ?>
-    <?= $form->field($model, 'status')->dropDownList(\app\models\News::getStatuses()) ?>
+/**
+ * @var yii\web\View $this
+ * @var app\models\News $model
+ * @var yii\widgets\ActiveForm $form
+ */
+
+\app\assets\MarkdownEditorAsset::register($this);
+?>
+
+<?php $form = ActiveForm::begin(['id' => 'news-form']) ?>
+
+<?= $form->field($model, 'title')->textInput(['maxlength' => 50]) ?>
+<?= $form->field($model, 'text')->textarea() ?>
+
+<?= $form->field($model, 'link')->textInput(['maxlength' => 255]) ?>
+<?= $form->field($model, 'status')->dropDownList(\app\models\News::getStatuses()) ?>
+
     <div class="form-group">
         <?= Html::submitButton(Yii::t('news', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
-    <?php ActiveForm::end(); ?>
+
+<?php ActiveForm::end() ?>
