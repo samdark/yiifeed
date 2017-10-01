f27b0985fcf375cb7f22ecb324d331585c645680
Nikola Trifunovic <johonunu@gmail.com>
2017-10-01 23:16:22 +0200

    login and signup design implemented

--- views/site/login.php
+++ views/site/login.php
@@ -1,6 +1,7 @@
 <?php
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use yii\widgets\Breadcrumbs;
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
@@ -9,20 +10,14 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-
-<?php $this->beginBlock('header'); ?>
-<div class="header-title"> 
-<div class="container">
-    <h4><?= $this->title ?></h4>
-</div>
-</div>
-<?php $this->endBlock(); ?>
-
 <div class="site-login">
 
     <div class="row">
         
-        <div class="col-sm-4 col-sm-offset-2">
+        <div class="col-sm-4 col-sm-offset-1">
+            <?= Breadcrumbs::widget([
+                'links' => $this->params['breadcrumbs'],
+            ]) ?>
             <h2>Fill out the following form</h2>
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
                 <?= $form->field($model, 'username')->textInput(['class'=>'form-control input-lg']) ?>
@@ -35,11 +30,15 @@ $this->params['breadcrumbs'][] = $this->title;
             <p><?= Html::a('Forgot your password ?', ['site/request-password-reset']) ?></p>
         </div>
         
-        <div class="col-sm-4 col-sm-offset-1 text-center well github-well">
+        <div class="col-sm-4 col-sm-offset-2 text-center github-side">
             <i class="fa fa-github fa-5x" aria-hidden="true"></i>
             <h3>Did you sign up with your<br>Github Account?</h3>
             <?= Html::a('Login with Github', ['/site/auth', 'authclient' => 'github'],['class' => 'btn btn-primary btn-lg btn-block']);?>
             <p class="text-muted">To connect your existing account<br>with Github, log in first.</p>
         </div>
     </div>
+</div>
+
+<div class="github-background">
+    <div class="arrow"></div>
 </div>
--- views/site/signup.php
+++ views/site/signup.php
@@ -1,6 +1,7 @@
 <?php
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use yii\widgets\Breadcrumbs;
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
@@ -10,19 +11,12 @@ $this->title = 'Signup';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<?php $this->beginBlock('header'); ?>
-<div class="header-title"> 
-<div class="container">
-    <h4><?= $this->title ?></h4>
-</div>
-</div>
-<?php $this->endBlock(); ?>
-
-
 <div class="site-signup">
     <div class="row">
-        
-        <div class="col-sm-4 col-sm-offset-2">
+        <div class="col-sm-4 col-sm-offset-1">
+            <?= Breadcrumbs::widget([
+                'links' => $this->params['breadcrumbs'],
+            ]) ?>
             <h2>Fill out the following form</h2>
             <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
                 <?= $form->field($model, 'username')->textInput(['class'=>'form-control input-lg']) ?>
@@ -34,7 +28,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <?php ActiveForm::end(); ?>
         </div>
 
-        <div class="col-sm-4 col-sm-offset-1 text-center well github-well">
+        <div class="col-sm-4 col-sm-offset-2 text-center github-side">
             <i class="fa fa-github fa-5x" aria-hidden="true"></i>
             <h3>Did you sign up with your<br>Github Account?</h3>
             <?= Html::a('Login with Github', ['/site/auth', 'authclient' => 'github'],['class' => 'btn btn-primary btn-lg btn-block']);?>
@@ -42,4 +36,8 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
 
     </div>
+</div>
+
+<div class="github-background">
+    <div class="arrow"></div>
 </div>
